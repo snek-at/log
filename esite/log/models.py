11a809caeaa6e9b3db26bfa3ed5e7e6437f1f1ca
sxct <nicoschett@icloud.com>
2020-05-05 14:20:19 +0200

    Some formatting and new starttime field
    A new model field has been added.

--- esite/log/models.py
+++ esite/log/models.py
@@ -9,6 +9,7 @@ from wagtail.admin.edit_handlers import StreamFieldPanel, MultiFieldPanel, Field
 #from .blocks import UserChooserBlock
 from .widgets import UserChooser
 
+
 class Workpackage(models.Model):
     STATUS = (
         ('new', 'Workpackage has not been started'),
@@ -19,55 +20,50 @@ class Workpackage(models.Model):
     )
 
     name = models.CharField(null=True, blank=False, max_length=255)
-    status = models.CharField(max_length=32,
+    status = models.CharField(
+        max_length=32,
         choices=STATUS,
         default='new',
     )
     durration = models.DurationField(null=True, blank=False)
+    # OUTDATED
     realtime = models.DurationField(null=True, blank=False, default='00:00:00')
-    sid = models.CharField(
-        max_length=3,
-        validators=[
-            RegexValidator(
-                regex='^\d{3}$',
-                message='ID doesnt comply',
-            ),
-        ],
-        default="000",
-        unique=True
-    )
-    did = models.CharField(
-        max_length=7,
-        validators=[
-            RegexValidator(
-                regex='^\d{3}\-\d{3}$',
-                message='ID doesnt comply',
-            ),
-        ],
-        default="000-000"
-    )
-    pid = models.CharField(
-        max_length=11,
-        validators=[
-            RegexValidator(
-                regex='^\d{3}\-\d{3}-\d{3}$',
-                message='ID doesnt comply',
-            ),
-        ],
-        default="000-000-000",
-        primary_key=True
-    )
+    starttime = models.DateTimeField(null=True, blank=False)
+    sid = models.CharField(max_length=3,
+                           validators=[
+                               RegexValidator(
+                                   regex='^\d{3}$',
+                                   message='ID doesnt comply',
+                               ),
+                           ],
+                           default="000",
+                           unique=True)
+    did = models.CharField(max_length=7,
+                           validators=[
+                               RegexValidator(
+                                   regex='^\d{3}\-\d{3}$',
+                                   message='ID doesnt comply',
+                               ),
+                           ],
+                           default="000-000")
+    pid = models.CharField(max_length=11,
+                           validators=[
+                               RegexValidator(
+                                   regex='^\d{3}\-\d{3}-\d{3}$',
+                                   message='ID doesnt comply',
+                               ),
+                           ],
+                           default="000-000-000",
+                           primary_key=True)
     start = models.TimeField(null=True, blank=False)
     end = models.TimeField(null=True, blank=False)
 
-    assoc_user = models.ForeignKey(
-        get_user_model(),
-        null=True,
-        blank=False,
-        on_delete=models.SET_NULL,
-        related_name='+',
-        verbose_name="Associated User"
-    )
+    assoc_user = models.ForeignKey(get_user_model(),
+                                   null=True,
+                                   blank=False,
+                                   on_delete=models.SET_NULL,
+                                   related_name='+',
+                                   verbose_name="Associated User")
 
     # assoc_user_list = StreamField([
     #     ('userchooser', UserChooserBlock(help_text="A user associated with the workpackage"))
@@ -80,15 +76,15 @@ class Workpackage(models.Model):
         FieldPanel('name'),
         FieldPanel('status'),
         FieldPanel('durration'),
+        FieldPanel('starttime'),
         FieldPanel('realtime'),
         FieldPanel('assoc_user', widget=UserChooser),
         #StreamFieldPanel(assoc_user_list)
     ]
 
-    
-
     def __str__(self):
         return self.name
+
 
 # SPDX-License-Identifier: (EUPL-1.2)
 # Copyright © 2020 miraculix-org Florian Kleber
