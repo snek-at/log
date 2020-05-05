4d83cd6be9c0ca12d33011ee78406048b348a3eb
sxct <nicoschett@icloud.com>
2020-05-05 11:47:16 +0200

    Enhance bot interface
    The interface for the telegram bot has been
    improved.

--- esite/log/apps.py
+++ esite/log/apps.py
@@ -16,11 +16,11 @@ import asyncio
 import threading
 import json
 import re
+import string
 
-#from esite.waveater.models import Log
-
-#log = Log.objects.create(uid=45350407,workpaket=[workpackage,])
 # BOX object
+
+
 class BOX:
     # meths
     @classmethod
@@ -35,104 +35,246 @@ class BOX:
     def commands(cls, cmd):
         # input: lowercase string splitted by ":"
         # output: [0] - the text message,[1] - buttons
-        cmd = cmd.split(": ")[0]
+        cmd = cmd.split(":")[0]
+        print(cmd)
         return {
+            'info': lambda: cls.info,
             'start': lambda: cls.start,
             'back': lambda: cls.start,
             'workpackages': lambda: cls.workpackages,
+            'todo': lambda: cls.todo,
             'wp': lambda: cls.workpackage,
+            'wp_start': lambda: cls.workpackage_start,
+            'wp_commit_handler': lambda: cls.workpackage_commit,
+            'wp_commit': lambda: cls.commit,
         }.get(cmd, lambda: cls.help)()
 
     @classmethod
+    def info(cls, **kwargs):
+        event = kwargs["event"]
+
+        text = f'Your TelegramID: {event.query.user_id}'
+
+        return (text, [
+                [Button.inline('Back')],
+                ])
+
+    @classmethod
     def start(cls, **kwargs):
-        return ("Back",[
-            [Button.inline('Workpackages'), Button.inline('Start')],
+        event = kwargs["event"]
+
+        from django.contrib.auth import get_user_model
+        from .models import Workpackage
+
+        users = get_user_model().objects.filter(telegram_id=event.query.user_id)
+        my_workpackages = []
+        if(len(users) > 0):
+            my_workpackages = Workpackage.objects.filter(
+                assoc_user=users[0], status="ongoing")
+        print("user")
+        print("USER:", users)
+
+        workpackages = Workpackage.objects.filter(status="ongoing")
+
+        """ Ongoing packages overview """
+        """
+        000-000-000 SNEKLOG RUNNING-TIME by schettn
+        000-000-001 SNEKLOG RUNNING-TIME by cisco
+        """
+        print("overivew")
+        overview = [
+            f'`{workpackage.pid} {workpackage.name} {workpackage.realtime} by {workpackage.assoc_user}`' for workpackage in workpackages]
+        overview_text = '`Ongoing Workpackages:`\n\n' + '\n'.join(overview)
+        print(overview_text)
+        # case1: a user have n ongoing packages
+        #   -> print a overview of all ongoing packages
+        #   -> Show workpackages button
+        #   -> Show my todo list
+        #
+        if(len(my_workpackages) > 0):
+            return (overview_text, [
+                [Button.inline('Workpackages', data=b'workpackages'),
+                 Button.inline('Todo', data=b'todo')], [Button.inline('Info')],
+            ])
+        else:
+            # case2: no pacakge of the current user is ongoing.
+            #   -> Print a overview of all ongoing packages
+            #   -> Show workpackages button
+            #
+            return (overview_text, [
+                [Button.inline('Workpackages', data=b'workpackages')], [Button.inline('Info')],
+            ])
+
+        return ("Back", [
+            [Button.inline('Workpackages', data=b'workpackages'),
+             Button.inline('Start')], [Button.inline('Info')],
         ])
 
     @classmethod
     def workpackages(cls, **kwargs):
         from .models import Workpackage
-
+        print(kwargs["event"])
         user_id = kwargs["event"].query.user_id
         workpackages = Workpackage.objects.all()
-        # user_id = kwargs["event"].query.user_id
-        # print('Hello')
-        # log = Log.objects.get(uid=user_id)
-        # print("USERIDDD", log.workpackages)
-        # workpackages_object = json.loads(log.workpackages)
-        # print(workpackages_object[0]['name'])
-        # print([[Button.inline(workpackage['name'])] for workpackage in workpackages_object])
-        button_list = [[Button.inline(f"wp: {workpackage.pid} {workpackage.name}")] for workpackage in workpackages]
-        print(button_list)
+        button_list = [[Button.inline(f"{workpackage.pid} {workpackage.name}", data=(
+            f"wp:{workpackage.pid}").encode())] for workpackage in workpackages]
         button_list.append([Button.inline('Back')])
-        return (f'Afoch irgendwos',
-            button_list
-        )
-    
+
+        new_count = Workpackage.objects.filter(status='new').count()
+        ongoing_count = Workpackage.objects.filter(status='ongoing').count()
+        waiting_count = Workpackage.objects.filter(status='waiting').count()
+        review_count = Workpackage.objects.filter(status='review').count()
+        fin_count = Workpackage.objects.filter(status='fin').count()
+
+        overview_text = f"`New: {new_count}\nOngoing: {ongoing_count}\nWaiting: {waiting_count}\nWaiting for Review: {review_count}\nFinished: {fin_count}`"
+
+        return (overview_text,
+                button_list
+                )
+
     @classmethod
     def workpackage(cls, **kwargs):
-        from .models import Workpackage
-        print(kwargs['event'])
-        user_id = kwargs['event'].query.user_id
+        print(kwargs)
+        event = kwargs["event"]
 
-        # workpackage = Workpackage.objects.get(uid=user_id)
-        data = kwargs['event'].query.data.decode("utf-8").lower()
+        from django.contrib.auth import get_user_model
+        from .models import Workpackage
+
+        user_id = event.query.user_id
+        data = event.query.data.decode("utf-8").lower()
         print(data)
-        pid = re.search('\d{3}\-\d{3}-\d{3}', data).group(0)
+        pid = data.split(":")[1]
+        print(pid)
         workpackage = Workpackage.objects.get(pid=pid)
-        print("WP", workpackage)
+        wp_out = f"`{workpackage.name}\nstatus: {workpackage.status}\ndurration: {workpackage.durration}\nrealtime: {workpackage.realtime}\nsid: {workpackage.sid}\ndid: {workpackage.did}\npid: {workpackage.pid}`"
+
+        users = get_user_model().objects.filter(telegram_id=event.query.user_id)
+        if(len(users) > 0):
+            workpackage_check = Workpackage.objects.filter(
+                pid=pid, assoc_user=users[0])
+            print(workpackage_check)
+            if(len(workpackage_check) > 0):
+                if(workpackage.status == 'new' or workpackage.status == 'waiting'):
+                    return (wp_out, [[Button.inline('Start', data=(f"wp_start:{workpackage.pid}").encode()), Button.inline('Back', data=(f"back:wp:{workpackage.pid}").encode())]])
+                if(workpackage.status == 'review' or workpackage.status == 'review'):
+                    return (wp_out, [[Button.inline('Back', data=(f"back:wp:{workpackage.pid}").encode())]])
+                return (wp_out, [[Button.inline('Commit', data=(f"wp_commit:{workpackage.pid}").encode())], [Button.inline('Back', data=(f"back:wp:{workpackage.pid}").encode())]])
+            print("false")
+        return (wp_out, [[Button.inline('Back', data=(f"back:wp:{workpackage.pid}").encode())]])
+
+        # state = ('Nothing', 'wp_nothing')
+        # if(user):
+        #     if(workpackage.status == 'new'):
+        #         state = ('Start', 'wp_start')
+        #     if(workpackage.status == 'ongoing'):
+        #         state = ('Stop', 'wp_stop')
+
+        # if(workpackage.status == 'fin')
+        #     pass#no button
+
+        # if(work)
+
+        # return (wp_out, [[Button.inline(state[0], data=(f"{state[1]}:{workpackage.pid}").encode()), Button.inline('Back', data=(f"back:wp:{workpackage.pid}").encode())]])
+    @classmethod
+    def todo(cls, **kwargs):
+        event = kwargs["event"]
+
+        from django.contrib.auth import get_user_model
+        from .models import Workpackage
+
+        users = get_user_model().objects.filter(telegram_id=event.query.user_id)
+        button_list = []
+        if(len(users) > 0):
+            workpackages = Workpackage.objects.filter(assoc_user=users[0])
+            button_list = [[Button.inline(f"[{workpackage.status}] {workpackage.pid} {workpackage.name} ", data=(
+                f"wp:{workpackage.pid}").encode())] for workpackage in workpackages]
+
+        button_list.append([Button.inline('Back')])
+
+        return (f'`Your workpackages:`',
+                button_list
+                )
+
+    @classmethod
+    def workpackage_start(cls, **kwargs):
+        print(kwargs)
+        event = kwargs['event']
+        from .models import Workpackage
+
+        data = event.query.data.decode("utf-8").lower()
+        pid = data.split(":")[1]
+
+        workpackage = Workpackage.objects.get(pid=pid)
+        workpackage.status = "ongoing"
+        workpackage.save()
 
         wp_out = f"`{workpackage.name}\nstatus: {workpackage.status}\ndurration: {workpackage.durration}\nrealtime: {workpackage.realtime}\nsid: {workpackage.sid}\ndid: {workpackage.did}\npid: {workpackage.pid}`"
-        return (wp_out, [[Button.inline('Back')]])
+        print("before crash")
+        return cls.workpackage(event=event)
+
+    @classmethod
+    def workpackage_commit(cls, **kwargs):
+        print(kwargs)
+        event = kwargs['event']
+        from .models import Workpackage
+
+        data = event.query.data.decode("utf-8").lower()
+        cmd, pid, status = data.split(":")
+
+        workpackage = Workpackage.objects.get(pid=pid)
+        workpackage.status = status
+        workpackage.save()
+
+        wp_out = f"`{workpackage.name}\nstatus: {workpackage.status}\ndurration: {workpackage.durration}\nrealtime: {workpackage.realtime}\nsid: {workpackage.sid}\ndid: {workpackage.did}\npid: {workpackage.pid}`"
+        print("before crash")
+        return cls.workpackage(event=event)
+
+    @classmethod
+    def commit(cls, **kwargs):
+        # Stop
+        # Note
+        # Request Review
+        print("HEYDIHOE")
+        event = kwargs['event']
+        print(event)
+        data = event.query.data.decode("utf-8").lower()
+        pid = data.split(":")[1]
+
+        return ("useable commands and arguments are", [
+            [Button.inline('Stop', data=(f"wp_commit_handler:{pid}:waiting").encode()),
+             Button.inline('Note', data=(f"wp_note:{pid}").encode())],
+            [Button.inline('Request Review', data=(
+                f"wp_commit_handler:{pid}:review").encode())],
+        ])
 
     @classmethod
     def help(cls, **kwargs):
-        return ("useable commands and arguments are",[
-            [Button.inline('Workpackages'), Button.inline('Start')],
+        return ("useable commands and arguments are", [
+            [Button.inline('Workpackages', data=b'workpackages'),
+             Button.inline('Start')],
         ])
-        #return ('useable commands and arguments are:\n\thelp\t--help -h\n\tadd\t--add -a\n\texit\t--exit -e', [])
+        # return ('useable commands and arguments are:\n\thelp\t--help -h\n\tadd\t--add -a\n\texit\t--exit -e', [])
 
 
 class LogConfig(AppConfig):
     name = 'esite.log'
 
     def ready(self):
-        #from esite.waveater.models import Log
         """Start the client."""
-        workpackage = {
-            "name": "init",
-            "status": "fertig",
-            "durration": "9h",
-            "realtime": "10h35m3s",
-            "sid": "1",
-            "did": "1.1",
-            "pid": "1.1.1",
-        }
-        workpackage2 = {
-            "name": "init2",
-            "status": "fertig",
-            "durration": "9h",
-            "realtime": "10h35m3s",
-            "sid": "1",
-            "did": "1.1",
-            "pid": "1.1.1",
-        }
-
-        # log = Log(uid=45350407, workpackages=json.dumps([workpackage,workpackage2 ]))
-        # log.save()
-        # log = Log(uid=903222659, workpackages=json.dumps([workpackage,workpackage2 ]))
-        # log.save()
         threading.Thread(name="log-main-thread", target=Log.main).start()
+
 
 class Log():
     def main():
         loop = asyncio.new_event_loop()
-        client = TelegramClient('anon', settings.TELEGRAM_API_ID, settings.TELEGRAM_API_HASH, loop=loop).start(bot_token=settings.TELEGRAM_BOT_TOKEN)
+        client = TelegramClient('anon', settings.TELEGRAM_API_ID, settings.TELEGRAM_API_HASH, loop=loop).start(
+            bot_token=settings.TELEGRAM_BOT_TOKEN)
 
         @client.on(events.CallbackQuery)
         async def callback(event):
             data = event.query.data.decode("utf-8").lower()
-            cmd_out= BOX.commands(data)(event=event)
+            print(data)
+            cmd_out = BOX.commands(data)(event=event)
             # print(workpackages)
             # if(workpackages):
             #     print("true1")
@@ -143,12 +285,12 @@ class Log():
             #     cmd_out= BOX.commands(data)(event)
             #     print("false")
 
-            #workpackages = cmd_out[3]           
-            #print(event.query.user_id)
-            #print(event.query.__dict__.keys())
-            #print(event.query.data.decode("utf-8"))
-            #print(BOX.commands(event.query.data.decode("utf-8").lower())()[0])
-            #print(cmd_out)
+            #workpackages = cmd_out[3]
+            # print(event.query.user_id)
+            # print(event.query.__dict__.keys())
+            # print(event.query.data.decode("utf-8"))
+            # print(BOX.commands(event.query.data.decode("utf-8").lower())()[0])
+            # print(cmd_out)
             await client.edit_message(event.chat_id, event.query.msg_id, cmd_out[0], buttons=cmd_out[1])
 
         def funcname(parameter_list):
@@ -161,11 +303,11 @@ class Log():
             await event.respond("Hi, I'm an audio slave! :3\nI would love to convert every wav you got into a telegram voice message. (>.<)")
             raise events.StopPropagation
 
-        @client.on(events.NewMessage(pattern='/main'))
+        @client.on(events.NewMessage(pattern='/init'))
         async def start(event):
             """Send a message when the command /start is issued."""
-            cmd_out= BOX.commands('start')()
-            await event.respond('Pick one from this grid', buttons= cmd_out[1])
+            cmd_out = BOX.commands('start')()
+            await event.respond('Pick one from this grid', buttons=cmd_out[1])
 
             raise events.StopPropagation
 
@@ -191,15 +333,15 @@ class Log():
                     # audio_out = io.BytesIO()
                     # audio_out = audio_seg.export(audio_out, bitrate="128k", format='ogg', codec="opus", parameters=["-strict", "-2", "-ac", "2", "-vol", "150"], tags={"ARTIST": "waveater", "GENRE": "Meeting/Trashtalk", "ALBUM": "waveater", "TITLE": f"{event.message.file.name.split('.')[0]}_{event.message.media.document.date.strftime('%m-%d_%H-%M')}", "DATE": f"{event.message.media.document.date.strftime('%Y/%m/%d_%H:%M:%S')}", "COMMENT": f"A wav file converted to telegram voice message.", "CONTACT":"@waveater"})
                     # audio_out.name = f"{event.message.file.name.split('.')[0]}_{event.message.media.document.date.strftime('%m-%d_%H-%M')}.ogg"
-                    #print(len(audio_seg)//1000)
-                    #print(event.message.file.id)
-                    #print(event.message.file.title)
-                    #print(event.message.file.performer)
-                    #print(event.message.file.name)
-                    #print(event.message.file.duration)
+                    # print(len(audio_seg)//1000)
+                    # print(event.message.file.id)
+                    # print(event.message.file.title)
+                    # print(event.message.file.performer)
+                    # print(event.message.file.name)
+                    # print(event.message.file.duration)
                     result = await client.send_file(event.chat_id, audio_out, voice_note=True, caption=f"{event.message.message}\n\n`track: '{event.message.file.name.split('.')[0]}_{event.message.media.document.date.strftime('%m-%d_%H-%M')}',\nchannel: '{audio_seg.channels}'',\nformat: 'ogg',\ncodec: 'opus',\nbitrate: '128k'`", reply_to=event.message)
-                    #print(result.file.duration)
-                    #print(result.file.performer)
+                    # print(result.file.duration)
+                    # print(result.file.performer)
 
                     await msg.delete()
 
@@ -207,7 +349,7 @@ class Log():
                     print(e)
                     await msg.edit(f"OwO Shit happens! Something has gone wrong.\n\n**Error:** {e}")
 
-        #print(f"{threading.enumerate()}")
+        # print(f"{threading.enumerate()}")
         client.run_until_disconnected()
 
 # SPDX-License-Identifier: (EUPL-1.2)
